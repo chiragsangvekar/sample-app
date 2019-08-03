27a9429c210b4a953005f00d75dd0235cb7ae15f
Chirag <chirag@Snehit-MacBook.local>
2019-08-03 21:17:38 +0530

    startActivityForResult callbacks

--- app/src/main/java/com/example/sampleapplication/Activity2.kt
+++ app/src/main/java/com/example/sampleapplication/Activity2.kt
@@ -1,4 +1,62 @@
 package com.example.sampleapplication
 
-class Activity2 {
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+
+class Activity2: AppCompatActivity() {
+    var match : Boolean = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+        Log.d("ActivityCallbacks","A2 create")
+
+        if(intent.action.equals(Intent.ACTION_SEND)) {
+                match = true
+            Log.d("ActivityCallbacks","A2 match = true")
+            }
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityCallbacks","A2 destroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityCallbacks","A2 pause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityCallbacks","A2 stop")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityCallbacks","A2 resume")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityCallbacks","A2 start")
+
+    }
+
+    fun sendToPreviousActivity(view : View){
+        val resultIntent = Intent()
+        Log.d("ActivityCallbacks","A2 sendToPreviousActivity")
+// To pass any data to next activity
+// start your next activit
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+
+    }
 }
--- app/src/main/java/com/example/sampleapplication/MainActivity.kt
+++ app/src/main/java/com/example/sampleapplication/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.sampleapplication
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -13,7 +16,9 @@ import com.example.sampleapplication.databinding.ActivityMainBinding
 import com.example.sampleapplication.db.AppDatabase
 import com.example.sampleapplication.db.MyData
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.experimental.launch
+//import kotlinx.coroutines.experimental.channels.Send
+
+//import kotlinx.coroutines.experimental.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,22 +34,51 @@ class MainActivity : AppCompatActivity() {
     fun initUI() {
         val dataDao = AppDatabase.get(applicationContext).myDataDao()
         viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
-        viewModel.dataListResultUIModel.observe(this, Observer { listData ->
+//        viewModel.dataListResultUIModel.observe(this, Observer { listData ->
+//
+//            binding.listRv.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
+//            binding.listRv.adapter = DataListAdapter()
+//            var adapter = binding.listRv.adapter as DataListAdapter
+//            listData.let {
+//                adapter.updateData(listData)
+//            }
+//
+//            launch {
+//                dataDao.insertAll(transformToDBDAta(listData))
+//                Log.d("MainActivityTest","Entry: " + dataDao.findData(1))
+//            }
+//        })
 
-            binding.listRv.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
-            binding.listRv.adapter = DataListAdapter()
-            var adapter = binding.listRv.adapter as DataListAdapter
-            listData.let {
-                adapter.updateData(listData)
-            }
+//        viewModel.getData()
 
-            launch {
-                dataDao.insertAll(transformToDBDAta(listData))
-                Log.d("MainActivityTest","Entry: " + dataDao.findData(1))
-            }
-        })
+    }
 
-        viewModel.getData()
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityCallbacks","A1 destroy")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityCallbacks","A1 pause")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityCallbacks","A1 stop")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityCallbacks","A1 resume")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityCallbacks","A1 start")
 
     }
 
@@ -57,5 +91,23 @@ class MainActivity : AppCompatActivity() {
                 data = it.url))
         }
         return myData
+    }
+
+    fun startNewActivity(view : View){
+        val intent = Intent(this, Activity2::class.java)
+// To pass any data to next activity
+        intent.setAction(Intent.ACTION_SEND)
+        intent.putExtra("keyIdentifier", "Hello")
+// start your next activity
+        startActivityForResult(intent,1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        Log.d("ActivityCallbacks","A1 onActivityResult")
+        if(resultCode == Activity.RESULT_OK){
+            Log.d("ActivityCallbacks","A1 onActivityResult success")
+        }
+
     }
 }
