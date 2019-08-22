93fa3f79d73cf2abdbfe7f0921c975b3f4566de9
Chirag Sangvekar <sangvekar.chirag1994@gmail.com>
2019-08-22 23:59:53 +0530

    api change

--- app/src/main/java/com/example/sampleapplication/Network/RetrofitServices.kt
+++ app/src/main/java/com/example/sampleapplication/Network/RetrofitServices.kt
@@ -7,7 +7,7 @@ import retrofit2.http.Query
 
 interface RetrofitServices {
 
-    @GET("/customsearch/v1?q=harrypotter&cx=011476162607576381860:ra4vmliv9ti&key=AIzaSyDpMYRjzmp67tQoGDmCk8iun_rY657Lefs")
+    @GET("/customsearch/v1?cx=011476162607576381860:ra4vmliv9ti&key=AIzaSyDpMYRjzmp67tQoGDmCk8iun_rY657Lefs")
     fun getDataList(@Query("q") searchText: String): Call<ImageNwModel>
 
 }
